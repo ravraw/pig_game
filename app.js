a037ecee4f9fbf0f29553fa944feb97d455143a3
ravindra rawat <ravinrawat.wd@gmail.com>
2018-04-23 03:10:25 +0000

    game complete

--- app.js
+++ app.js
@@ -10,14 +10,15 @@ GAME RULES:
 */
 // return a random number between 1 - 6
 
-///// player construction
+/////////////// PLAYER CONSTRUCTION  //////////////////////
+
 function Player(playerName, currentScore, totalScore, active) {
   this.playerName = playerName;
   this.currentScore = currentScore;
   this.totalScore = totalScore;
   this.active = active;
   this.winner = function() {
-    if (this.totalscore >= 100) {
+    if (this.totalScore >= 100) {
       return true;
     } else {
       return false;
@@ -28,47 +29,48 @@ function Player(playerName, currentScore, totalScore, active) {
 const player1 = new Player("player1", 0, 0, true);
 const player2 = new Player("player2", 0, 0, false);
 
-// variable declarations
+/////////////// VARIABLE DECLARATIONS  ////////////////////
 
-const dice = document.querySelector(".dice");
-const btnRollDice = document.querySelector(".btn-roll");
-const btnHold = document.querySelector(".btn-hold");
 const btnNew = document.querySelector(".btn-new");
+const btnRollDice = btnNew.nextElementSibling;
+const btnHold = btnRollDice.nextElementSibling;
+const dice = btnHold.nextElementSibling;
 
 const currentScore_player1 = document.querySelector("#current-0");
 const currentScore_player2 = document.querySelector("#current-1");
 const totalScore_player1 = document.querySelector("#score-0");
 const totalScore_player2 = document.querySelector("#score-1");
-const winner_player1 = document.querySelector("#name-0");
-const winner_player2 = document.querySelector("#name-1");
+const panel_player1 = document.querySelector(".player-0-panel");
+const panel_player2 = document.querySelector(".player-1-panel");
 
-// function decalarations
+const name_player1 = document.querySelector("#name-0");
+const name_player2 = document.querySelector("#name-1");
+
+/////////////////  FUNCTIONS DECLARATIONS ////////////////////////
+
+///// Function genertes a random number and displays the image accordingly.
 
 function rollDice() {
   let number = Math.ceil(Math.random() * 6);
   let image = `dice-${number}.png`;
-  dice.src = image;
+  dice.src = image; // image is replaced .
   console.log(number);
   return number;
 }
 
-function checkWinner() {
-  if (player1.totalScore >= 100) {
-    winner_player1.innerHTML = "Winner";
-  }
-  if (player2.totalScore >= 100) {
-    winner_player2.innerHTML = "Winner";
-  }
+/////// Change turn
+
+function changeTurn() {
+  panel_player1.classList.toggle("active");
+  panel_player2.classList.toggle("active");
 }
 
-// Event listeners
+/////// Function updates current scores after each dice roll.
 
-btnRollDice.addEventListener("click", () => {
-  let numberRolled = rollDice();
-  console.log(numberRolled);
-
+function updateCurrentScores() {
+  let numberRolled = rollDice(); // randomnumber and image generated
   if (numberRolled !== 1) {
-    if (player1.active === true) {
+    if (panel_player1.classList.contains("active")) {
       player1.currentScore += numberRolled;
       currentScore_player1.innerHTML = player1.currentScore;
     } else {
@@ -76,43 +78,70 @@ btnRollDice.addEventListener("click", () => {
       currentScore_player2.innerHTML = player2.currentScore;
     }
   } else {
-    if (player1.active === true) {
+    if (panel_player1.classList.contains("active")) {
       player1.currentScore = 0;
       currentScore_player1.innerHTML = player1.currentScore;
-      player1.active = false;
-      player2.active = true;
+      changeTurn();
     } else {
       player2.currentScore = 0;
       currentScore_player2.innerHTML = player2.currentScore;
-      player1.active = true;
-      player2.active = false;
+      changeTurn();
     }
   }
-});
+}
 
-btnHold.addEventListener("click", () => {
-  if (player1.active === true) {
+///////// Function updates Total score after each hold
+
+function updateTotalScores() {
+  if (panel_player1.classList.contains("active")) {
     player1.totalScore += player1.currentScore;
     totalScore_player1.innerHTML = player1.totalScore;
-    checkWinner();
     player1.currentScore = 0;
     currentScore_player1.innerHTML = player1.currentScore;
-    player1.active = false;
-    player2.active = true;
+
+    changeTurn();
   } else {
     player2.totalScore += player2.currentScore;
     totalScore_player2.innerHTML = player2.totalScore;
-    checkWinner();
     player2.currentScore = 0;
     currentScore_player2.innerHTML = player2.currentScore;
-    player2.active = false;
-    player1.active = true;
+
+    changeTurn();
   }
+}
+
+//////// Check for a possible winner after each hold.
+
+function checkWinner() {
+  if (player1.winner() === true) {
+    name_player1.innerHTML = "Winner";
+    name_player1.classList.add("winner");
+    panel_player1.classList.add("winner");
+  }
+  if (player2.winner() === true) {
+    name_player2.innerHTML = "Winner";
+    name_player2.classList.add("winner");
+    panel_player2.classList.add("winner");
+  }
+}
+
+////////////////////////// EVENT LISTENERS ////////////////////
+
+//////// Dice roll
+btnRollDice.addEventListener("click", () => {
+  updateCurrentScores();
 });
 
+////////  Hold button clicked
+
+btnHold.addEventListener("click", () => {
+  updateTotalScores();
+  checkWinner();
+});
+
+////// Game reset
+
 btnNew.addEventListener("click", () => {
-  player1.active = true;
-  player2.active = false;
   player1.totalScore = 0;
   player1.currentScore = 0;
   player2.totalScore = 0;
@@ -121,4 +150,11 @@ btnNew.addEventListener("click", () => {
   currentScore_player2.innerHTML = player2.currentScore;
   totalScore_player1.innerHTML = player1.totalScore;
   totalScore_player2.innerHTML = player2.totalScore;
+
+  name_player1.innerHTML = "PLAYER 1";
+  name_player1.classList.remove("winner");
+  panel_player1.classList.remove("winner");
+  name_player2.innerHTML = "PLAYER 2";
+  name_player2.classList.remove("winner");
+  panel_player2.classList.remove("winner");
 });
